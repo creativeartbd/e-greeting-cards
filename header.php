d0f85ef963383a7d1c60ce97c182f72e2bd47e51
creativeartbd <creativeartbd@gmail.com>
2022-05-28 23:13:32 +0600

    Update front page logo link

--- header.php
+++ header.php
@@ -20,7 +20,7 @@ require_once 'admin/helper/functions.php';
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand" href="dashboard.php">eGreeting Cards</a>
+            <a class="navbar-brand" href="index.php">eGreeting Cards</a>
         </div>
     </nav>
 </div>
