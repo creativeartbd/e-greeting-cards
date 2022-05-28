ca88f3e8985067006f8e08d52201a6ec86e5e35c
creativeartbd <creativeartbd@gmail.com>
2022-05-28 23:15:51 +0600

    Update jQuery link

--- admin/footer.php
+++ admin/footer.php
@@ -1,5 +1,4 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="assets/js/jquery.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 <script src="assets/js/main.js"></script>
 </body>
